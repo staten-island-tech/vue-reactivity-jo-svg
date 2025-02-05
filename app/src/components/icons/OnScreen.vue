9586edb7a1b2e4f61c5a3cc3abfa09a5634e10a2
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-02-05 12:29:55 -0500

    sadas

--- app/src/components/icons/OnScreen.vue
+++ app/src/components/icons/OnScreen.vue
@@ -2,20 +2,20 @@
     <!-- the key allows vue to track the cards --> 
     <div class="view w-full bg-white flex-1 border-t-3 border-[#6B3B56] rounded-b-3xl overflow-hidden">
         <div class="relative mx-auto mt-[3rem] w-[27rem] h-[27rem] flex justify-center items-center ">
-        <img 
+          <img 
             v-for="item in onScreenItems" 
             :key="item.id" 
             :src="item.image" 
             :alt="item.name" 
             class="w-full absolute left-1/2 transform -translate-x-1/2 z-10"
-        />
+          />
         </div>
+
     </div>
 
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue';
 
 const props = defineProps({
   onScreenItems: {
